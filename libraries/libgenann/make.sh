808a224699c26fcd49406accfbeff0db9c371a24
AgoeZero <johnadjeiagoe@gmail.com>
2025-10-23 01:57:37 +0100

    Delete libraries/libgenann directory

--- libraries/libgenann/make.sh
+++
@@ -1,7 +0,0 @@
-rm *.o 2> /dev/null
-veval "$SYS_CC -c genann.c genann.o"
-asserterror $? "compilation failed"
-veval "$SYS_AR rcs $SYS_PREFIX/lib/libgenann.a genann.o"
-asserterror $? "ar failed"
-
-cp genann.h $SYS_PREFIX/include
